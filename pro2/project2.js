0d0ec2ed5eb054df9ae110108152b486b133497e
prathameshkpai <prathameshkow@gmail.com>
2018-12-14 16:53:51 +0530

    updated project2

--- pro2/project2.js
+++ pro2/project2.js
@@ -3,8 +3,7 @@ const request = require('request');
 request('https://ghibliapi.herokuapp.com/films', (error, response, body) => 
 {
     console.log('error:', error); // Print the error 
-    console.log('statusCode:', response && response.statusCode );  // print the status code
-    let Res = JSON.parse(body) 
-    //console.log('body:', body);   
-    console.log(Res)
+    console.log('statusCode:', response && response.statusCode);  // print the status code
+    console.log('body:', body);
+
 });
